872b4e8dc2e3bebb27b62ce7b50fca18c448871d
tmt018 <valentontimothy@gmail.com>
2022-05-28 21:17:38 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,252 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div className="container">
+      <a className="navbar-brand js-scroll-trigger" href="#page-top">My Studio</a>
+      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i className="fa fa-bars"></i>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav text-uppercase ml-auto">
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#team">Team</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  
+  <header className="masthead">
+    <div className="container">
+      <div className="intro-text">
+        <div className="intro-lead-in">Hello and welcome to our studio!</div>
+        <div className="intro-heading text-uppercase">It's exciting to meet you</div>
+        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+      </div>
+    </div>
+  </header>
+
+  
+  <section className="page-section" id="services">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Our Services</h2>
+          <h3 className="section-subheading text-light">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg>
+          <h4 className="service-heading">E-Commerce</h4>
+          <p className="text-muted">We offer the use of social media to create digital content and to advertise a product and build their business via all of the following digital platforms.</p>
+        </div>
+        <div className="col-md-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
+  <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
+</svg>
+          <h4 className="service-heading">Responsive Design</h4>
+          <p className="text-muted">We create a design that focuses on creating a flexible website with content and design components that alter as the user moves around the screen.</p>
+        </div>
+        <div className="col-md-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-file-lock2" viewBox="0 0 16 16">
+  <path d="M8 5a1 1 0 0 1 1 1v1H7V6a1 1 0 0 1 1-1zm2 2.076V6a2 2 0 1 0-4 0v1.076c-.54.166-1 .597-1 1.224v2.4c0 .816.781 1.3 1.5 1.3h3c.719 0 1.5-.484 1.5-1.3V8.3c0-.627-.46-1.058-1-1.224z"/>
+  <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+</svg>
+          <h4 className="service-heading">Web Security</h4>
+          <p className="text-muted">We aim to ensure that all information and knowledge required to support the security of a company's network and information systems is available safely and securely.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section className="bg-light page-section" id="team">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
+          <h3 className="section-subheading text-light">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/1.jpg" alt=""/>
+            <h4>Daniel Lopez</h4>
+            <p className="text-muted">Creative Director</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-facebook"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/2.jpg" alt=""/>
+            <h4>Cherry Lefebre</h4>
+            <p className="text-muted">Marketing Specialist</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-facebook"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="img/3.jpg" alt=""/>
+            <h4>Tracey Jenkins</h4>
+            <p className="text-muted">Project Manager</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-facebook"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-8 mx-auto text-center">
+          <p className="large text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section className="page-section" id="contact">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12 text-center">
+          <h2 className="section-heading text-uppercase">Contact Us</h2>
+          <h3 className="section-subheading text-dark">Lorem ipsum dolor sit amet consectetur.</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12">
+          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number."/>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+              </div>
+              <div className="clearfix"></div>
+              <div className="col-lg-12 text-center">
+                <div id="success"></div>
+                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+  <footer className="footer">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-md-4">
+          <span className="copyright">Copyright &copy; My Studio 2022</span>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline social-buttons">
+          <li className="list-inline-item">
+              <a href="#something">
+                <i className="bi bi-facebook"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">
+                <i className="bi bi-twitter"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+                <a href="#something">
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline quicklinks">
+            <li className="list-inline-item">
+              <a href="#something">Privacy Policy</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#something">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
     </div>
   );
 }
